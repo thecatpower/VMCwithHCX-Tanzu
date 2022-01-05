77e0145860cece965543af548e914bdea8e880d2
mconcilio <matteo@matteoconcilio.com>
2022-01-05 12:43:11 +0100

    Clone 1st VM template on cloud

--- Step 4 - VMs/main.tf
+++ Step 4 - VMs/main.tf
@@ -32,6 +32,7 @@ provider "vsphere" {
   allow_unverified_ssl  = true
 }
 
+/*
 provider "vsphere" {
   alias                 = "onprem"
   user                  = var.vc_onprem_user
@@ -39,7 +40,7 @@ provider "vsphere" {
   vsphere_server        = var.vc_onprem_url
   allow_unverified_ssl  = true
 }
-
+*/
 
 /*================
 Deploy Virtual Machimes
@@ -54,6 +55,116 @@ module "VMs" {
   compute_pool        = var.compute_pool
   Subnet12            = data.terraform_remote_state.phase2.outputs.segment12_name
   Subnet13            = data.terraform_remote_state.phase2.outputs.segment13_name
-
- 
 }*/
+
+data "vsphere_datacenter" "cloud_dc" {
+  name          = var.cloud_data_center
+}
+data "vsphere_compute_cluster" "cloud_cluster" {
+  name          = var.cloud_cluster
+  datacenter_id = data.vsphere_datacenter.cloud_dc.id
+}
+data "vsphere_datastore" "cloud_datastore" {
+  name          = var.cloud_workload_datastore
+  datacenter_id = data.vsphere_datacenter.cloud_dc.id
+}
+data "vsphere_resource_pool" "cloud_pool" {
+  name          = var.cloud_compute_pool
+  datacenter_id = data.vsphere_datacenter.cloud_dc.id
+}
+data "vsphere_network" "cloud_network1" {
+  name          = var.cloud_subnets.Name1
+  datacenter_id = data.vsphere_datacenter.cloud_dc.id
+}
+
+/*
+data "vsphere_datacenter" "onprem_dc" {
+  provider      = vsphere.onprem
+  name          = var.onprem_data_center
+}
+data "vsphere_compute_cluster" "onprem_cluster" {
+  provider      = vsphere.onprem
+  name          = var.onprem_cluster
+  datacenter_id = data.vsphere_datacenter.onprem_dc.id
+}
+data "vsphere_datastore" "onprem_datastore" {
+  provider      = vsphere.onprem
+  name          = var.onprem_workload_datastore
+  datacenter_id = data.vsphere_datacenter.onprem_dc.id
+}
+data "vsphere_resource_pool" "onprem_pool" {
+  provider      = vsphere.onprem
+  name          = var.onprem_compute_pool
+  datacenter_id = data.vsphere_datacenter.onprem_dc.id
+}
+data "vsphere_network" "onprem_network1" {
+  provider      = vsphere.onprem
+  name          = var.onprem_subnets.Subnet1
+  datacenter_id = data.vsphere_datacenter.onprem_dc.id
+}
+*/
+
+
+/*=================================================================
+Get Templates data
+==================================================================*/
+
+data "vsphere_virtual_machine" "cloud_VM_BE" {
+  name          = var.cloud_VM_BE
+  datacenter_id = data.vsphere_datacenter.cloud_dc.id
+}
+/*data "vsphere_virtual_machine" "onprem_FE" {
+  provider      = vsphere.onprem
+  name          = "FE-NGINX"
+  datacenter_id = data.vsphere_datacenter.onprem_dc.id
+}*/
+
+# ================================================
+resource "vsphere_virtual_machine" "cloud-vm1" {
+  name             = "terraform-BE"
+  resource_pool_id = data.vsphere_resource_pool.cloud_pool.id
+  datastore_id     = data.vsphere_datastore.cloud_datastore.id
+  num_cpus = 2
+  memory   = 4096
+  guest_id = "ubuntu64Guest"
+  network_interface {
+    network_id = data.vsphere_network.cloud_network1.id
+  }
+  disk {
+    label = "disk0"
+    size  = 16
+  }
+  disk {
+    label = "disk1"
+    size  = 50
+    unit_number=1
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.cloud_VM_BE.id
+  }
+}
+/*
+resource "vsphere_virtual_machine" "vm2" {
+  provider      = vsphere.onprem
+  name             = "terraform-photo"
+  resource_pool_id = data.vsphere_resource_pool.pool.id
+  datastore_id     = data.vsphere_datastore.datastore.id
+
+  num_cpus = 2
+  memory   = 1024
+  guest_id = "ubuntu64Guest"
+
+  network_interface {
+    network_id = data.vsphere_network.network13.id
+  }
+
+  disk {
+    label = "disk0"
+    size  = 20
+    thin_provisioned = false
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.photo.id
+  }
+}
+*/
