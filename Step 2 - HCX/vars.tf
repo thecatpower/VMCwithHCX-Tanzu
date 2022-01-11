6f572850323e9a3bc9dac526287026488fafbddf
cdelbosc <charlotte.delbosc@gmail.com>
2022-01-11 18:08:17 +0100

    UPDT vars for step 2 TEST DEPLOYMENT

---
+++ Step 2 - HCX/vars.tf
@@ -0,0 +1,23 @@
+variable "api_token" {
+  description = "API token used to authenticate when calling the VMware Cloud Services API."
+}
+
+variable "sddc_name"{
+  description = "Name of SDDC."
+}
+
+variable "sddc_id"{
+  description = "ID of SDDC."
+}
+
+variable "api_token_target" {
+  description = "API token used to authenticate when calling the VMware Cloud Services API."
+}
+
+variable "sddc_name_target"{
+  description = "Name of target SDDC."
+}
+
+variable "sddc_id_target"{
+  description = "ID of target SDDC."
+}
