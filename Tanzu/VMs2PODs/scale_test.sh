aa3b5f07f8d8f29203be1ddf4297ad0c246e7174
Brice Dereims <bdereims@gmail.com>
2022-01-18 19:01:43 +0100

    lab day2

--- Tanzu/VMs2PODs/scale_test.sh
+++ Tanzu/VMs2PODs/scale_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-URL=
+URL=$(kubectl -n ${USER} get svc -o json | jq '.items[] | select(.metadata.name=="nginx-service") | .status.loadBalancer.ingress[].ip' | sed "s/\"//g")
 
 while true
 do
-	wget -q -O- http://10.10.2.4/list.php
+	wget -q -O- http://${URL}/list.php
 done
