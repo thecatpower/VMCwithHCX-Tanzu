107a80f39e65a0614953e40570caf7cbad407fda
mconcilio <matteo@matteoconcilio.com>
2022-01-24 08:03:30 +0100

    Dual Provider

--- Step 2 - HCX/main.tf
+++ Step 2 - HCX/main.tf
@@ -1,8 +1,8 @@
 ################### SOURCE ###################
 provider hcx {
    //hcx         = local.hcx_cloud1_url
-   //username    = "cloudadmin@vmc.local"
-   //password    = "kAHyG8Ipo3Y+-Xw"
+   //username    = var.user
+   //password    = var.pw
    vmc_token   = var.api_token
 }
 resource "hcx_vmc" "vmc_xpday" {  
@@ -21,111 +21,46 @@ resource "hcx_vmc" "vmc_xpday_target" {
     sddc_id = var.sddc_id_target
 }
 
-/*
-// Site Pairing from On-prem to Cloud --> C2C
-resource "hcx_site_pairing" "C2C" {
-    provider = hcx.onprem
-    url         = hcx_vmc.vmc_xpday.cloud_url
-    username    = "cloudadmin@vmc.local"
-    password    = "kAHyG8Ipo3Y+-Xw"
+################### MESH ###################
+provider hcx {
+   alias       = "src-pw"
+   hcx         = hcx_vmc.vmc_xpday.cloud_url
+   username    = var.user
+   password    = var.pw
+   //vmc_token   = var.api_token
 }
-/*
-
-
-
-/*
-// Site Pairing with the other cloud - Remote is HCX-C2C-2
-resource "hcx_site_pairing" "C2C1toC2C2" {
-    url         = "https://hcx.sddc-35-177-56-95.vmwarevmc.com"
-    username    = "cloudadmin@vmc.local"
-    password    = "changeme"
+provider hcx {
+   alias       = "target-pw"
+   hcx         = hcx_vmc.vmc_xpday_target
+   username    = var.user_target
+   password    = var.pw_target
+   //vmc_token   = var.api_token
 }
-
-resource "hcx_network_profile" "net_management" {
-  site_pairing    = hcx_site_pairing.C2C1toC2C2
-  vmc           = true
-
-  name          = "externalNetwork"
-  mtu           = 1500
-
-  ip_range {
-    start_address   = "18.132.147.242"
-    end_address     = "18.132.147.242"
-  }
-
-  ip_range {
-    start_address   = "18.168.66.74"
-    end_address     = "18.168.66.74"
-  }
-
-  ip_range {
-    start_address   = "18.134.93.71"
-    end_address     = "18.134.93.71"
-  }
-
-  ip_range {
-    start_address   = "18.132.159.50"
-    end_address     = "18.132.159.50"
-  }
-
-  ip_range {
-    start_address   = "35.178.52.104"
-    end_address     = "35.178.52.104"
-  }
-
-  ip_range {
-    start_address   = "18.132.31.109"
-    end_address     = "18.132.31.109"
-  }
-
-  gateway           = ""
-  prefix_length     = 0
-  primary_dns       = ""
-  secondary_dns     = ""
-  dns_suffix        = ""
+// Site Pairing from SRC to TARGET
+resource "hcx_site_pairing" "HCX_Pair_C2C" {
+    provider    = hcx.src-pw
+    url         = hcx_vmc.vmc_xpday_target.cloud_url
+    username    = var.user_target
+    password    = var.pw_target
 }
-
-data "hcx_compute_profile" "vmc_cp" {
-    vcenter       = hcx_site_pairing.C2C1toC2C2.local_vc
-    name          = "ComputeProfile(vcenter)"
-}
-
-resource "hcx_service_mesh" "service_mesh_1" {
+resource "hcx_service_mesh" "HCX_SM_CtoC" {
+  provider      = hcx.src-pw
   name                            = "c1toc2sm"
-  site_pairing                    = hcx_site_pairing.C2C1toC2C2
+  site_pairing                    = hcx_site_pairing.HCX_Pair_C2C
   local_compute_profile           = "ComputeProfile(vcenter)"
   remote_compute_profile          = "ComputeProfile(vcenter)"
-
   app_path_resiliency_enabled     = false
   tcp_flow_conditioning_enabled   = false
-
   uplink_max_bandwidth            = 10000
-
-  nb_appliances                   = 2 // Up to 16 L2 extension
-
-  service {
-    name                = "INTERCONNECT"
-  }
-
-  service {
-    name                = "VMOTION"
-  }
-
-  service {
-    name                = "BULK_MIGRATION"
-  }
-
-  service {
-    name                = "NETWORK_EXTENSION"
-  }
-
-  service {
-    name                = "DISASTER_RECOVERY"
-  }
-
+  nb_appliances                   = 1 // Up to 16 L2 extension
+  service {name = "INTERCONNECT"}
+  service {name = "VMOTION"}
+  service {name = "BULK_MIGRATION"}
+  service {name = "NETWORK_EXTENSION"}
+  service {name = "DISASTER_RECOVERY"}
 }
 
-
+/*
 resource "hcx_l2_extension" "ls1" {
   site_pairing                    = hcx_site_pairing.C2C1toC2C2
   service_mesh_id                 = hcx_service_mesh.service_mesh_1.id
@@ -251,159 +186,4 @@ output "compute_profile_vmc" {
 output "service_mesh_vmc" {
     value = hcx_service_mesh.service_mesh_1
 }
-
-
-
-resource "hcx_site_pairing" "onPrem2vmc" {
-    provider    = hcx.onprem
-    url         = "https://hcx.sddc-18-135-22-243.vmwarevmc.com"
-    username    = "cloudadmin@vmc.local"
-    password    = "changeme"
-
-}
-
-resource "hcx_network_profile" "net_onprem_management" {
-    provider        = hcx.onprem
-    site_pairing    = hcx_site_pairing.onPrem2vmc
-    network_name  = "hcx-management"
-    name          = "HCX-Management-profile"
-    mtu           = 1500
-
-    ip_range {
-        start_address   = "172.17.9.101"
-        end_address     = "172.17.9.103"
-    }
-
-    gateway           = "172.17.9.1"
-    prefix_length     = 24
-    primary_dns       = "172.17.9.1"
-    secondary_dns     = ""
-    dns_suffix        = "cpod-vcn.az-fkd.cloud-garage.net"
-}
-
-
-
-resource "hcx_network_profile" "net_onprem_uplink" {
-    provider        = hcx.onprem
-    site_pairing    = hcx_site_pairing.onPrem2vmc
-    network_name  = "hcx-uplink"
-    name          = "HCX-Uplink-profile"
-    mtu           = 1600
-
-    ip_range {
-        start_address   = "172.17.9.104"
-        end_address     = "172.17.9.106"
-    }
-
-
-    gateway           = "172.17.9.1"
-    prefix_length     = 24
-    primary_dns       = "172.17.9.1"
-    secondary_dns     = ""
-    dns_suffix        = "cpod-vcn.az-fkd.cloud-garage.net"
-}
-
-resource "hcx_network_profile" "net_onprem_vmotion" {
-    provider        = hcx.onprem
-    site_pairing    = hcx_site_pairing.onPrem2vmc
-    network_name  = "hcx-vmotion"
-    name          = "HCX-vMotion-profile"
-    mtu           = 1500
-
-    ip_range {
-        start_address   = "172.17.9.107"
-        end_address     = "172.17.9.109"
-    }
-
-    gateway           = "172.17.9.1"
-    prefix_length     = 24
-    primary_dns       = "172.17.9.1"
-    secondary_dns     = ""
-    dns_suffix        = "cpod-vcn.az-fkd.cloud-garage.net"
-}
-
-resource "hcx_compute_profile" "compute_profile_1" {
-    provider              = hcx.onprem
-    name                  = "comp1"
-    datacenter            = "cPod-VCN"
-    cluster               = "Cluster"
-    datastore             = "vsanDatastore"
-
-    management_network    = hcx_network_profile.net_onprem_management.id
-    replication_network   = hcx_network_profile.net_onprem_management.id
-    uplink_network        = hcx_network_profile.net_onprem_uplink.id
-    vmotion_network       = hcx_network_profile.net_onprem_vmotion.id
-    dvs                   = "dvs7"
-
-    service {
-        name                = "INTERCONNECT"
-    }
-
-    service {
-        name                = "WANOPT"
-    }
-
-    service {
-        name                = "VMOTION"
-    }
-
-    service {
-        name                = "BULK_MIGRATION"
-    }
-
-    service {
-        name                = "NETWORK_EXTENSION"
-    }
-
-    service {
-        name                = "DISASTER_RECOVERY"
-    }
-
-}
-
-resource "hcx_service_mesh" "onprem_sm1" {
-    provider                        = hcx.onprem
-    name                            = "sm1"
-    site_pairing                    = hcx_site_pairing.onPrem2vmc
-    local_compute_profile           = hcx_compute_profile.compute_profile_1.name
-    remote_compute_profile          = "ComputeProfile(vcenter)"
-
-    app_path_resiliency_enabled     = false
-    tcp_flow_conditioning_enabled   = false
-
-    uplink_max_bandwidth            = 10000
-
-    service {
-        name                = "INTERCONNECT"
-    }
-
-    service {
-        name                = "VMOTION"
-    }
-
-    service {
-        name                = "BULK_MIGRATION"
-    }
-
-    service {
-        name                = "NETWORK_EXTENSION"
-    }
-
-    service {
-        name                = "DISASTER_RECOVERY"
-    }
-
-}
-
-
-resource "hcx_l2_extension" "l2_vlan1902" {
-    provider                        = hcx.onprem
-    site_pairing                    = hcx_site_pairing.onPrem2vmc
-    service_mesh_id                 = hcx_service_mesh.onprem_sm1.id
-    source_network                  = "vlan1902"
-
-    destination_t1                  = "cgw"
-    gateway                         = "10.19.2.1"
-    netmask                         = "255.255.255.0"
-
-    */
+*/
--- Step 2 - HCX/vars.tf
+++ Step 2 - HCX/vars.tf
@@ -1,15 +1,18 @@
 variable "api_token" {
   description = "API token used to authenticate when calling the VMware Cloud Services API."
 }
-
 variable "sddc_name"{
   description = "Name of SDDC."
 }
-
 variable "sddc_id"{
   description = "ID of SDDC."
 }
-
+variable "user"{
+  description = ""
+}
+variable "pw"{
+  description = ""
+}
 variable "api_token_target" {
   description = "API token used to authenticate when calling the VMware Cloud Services API."
 }
@@ -17,7 +20,12 @@ variable "api_token_target" {
 variable "sddc_name_target"{
   description = "Name of target SDDC."
 }
-
 variable "sddc_id_target"{
   description = "ID of target SDDC."
+}
+variable "user_target"{
+  description = ""
+}
+variable "pw_target"{
+  description = ""
 }
