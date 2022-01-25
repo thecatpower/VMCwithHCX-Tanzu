5026517299758162e898de19c76ecb7ee551a1e5
Brice Dereims <bdereims@gmail.com>
2022-01-25 17:36:13 +0100

    some mods

--- Tanzu/tips/create-users.sh
+++ Tanzu/tips/create-users.sh
@@ -9,6 +9,9 @@ do
 	useradd -U -G docker -m -s /bin/bash ${USERNAME}
 	chpasswd <<<"${USERNAME}:${USERNAME}"
 	su - ${USERNAME} -c "git clone https://github.com/thecatpower/VMCwithHCX-Tanzu"
+	cp /home/grease-monkey/VMCwithHCX-Tanzu/Tanzu/env /home/${USERNAME}/VMCwithHCX-Tanzu/Tanzu/.
+	cp /home/grease-monkey/VMCwithHCX-Tanzu/Tanzu/password /home/${USERNAME}/VMCwithHCX-Tanzu/Tanzu/.
+	chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/VMCwithHCX-Tanzu/Tanzu/*
 done
 
 exit 0
